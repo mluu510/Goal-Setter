86b080e1c2307320e1a1445acd3cd0e677782d46
Minh and TJ <student@appacademy.io>
2014-02-28 17:46:19 -0800

    Before jump

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+
+    if params[:user_id]
+      @comment.commentable_id = params[:user_id].to_i
+      @comment.commentable_type = "User"
+    else
+      @comment.commentable_id = params[:goal_id].to_i
+      @comment.commentable_type = "Goal"
+    end
+
+    flash[:errors] = @comment.errors.full_messages unless @comment.save
+
+    redirect_to :back
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ GoalSetter::Application.routes.draw do
 
   resources :users, :only => [:new, :create, :show] do
     resources :goals, :only => [:new, :create]
+    resources :comments, :only => [:create]
   end
 
   resources :goals, :except => [:new, :create] do
-      put 'mark_as_complete'
-    end
+    put 'mark_as_complete'
+    resources :comments, :only => [:create]
+  end
 
   resource :session, :only => [:new, :create, :destroy]
 end
